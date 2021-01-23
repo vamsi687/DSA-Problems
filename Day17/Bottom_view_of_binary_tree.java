792afe46cb8ff1ab3edd5ded7647e6f202d28a35
vamsi687 <31959666+vamsi687@users.noreply.github.com>
2021-01-23 22:29:08 +0530

    Bottom View of Binary Tree

---
+++ Day17/Bottom_view_of_binary_tree.java
@@ -0,0 +1,217 @@
+/*
+Question :
+Given a binary tree, print the bottom view from left to right.
+A node is included in bottom view if it can be seen when we look at the tree from bottom.
+
+                      20
+                    /    \
+                  8       22
+                /   \        \
+              5      3       25
+                    /   \      
+                  10    14
+
+For the above tree, the bottom view is 5 10 3 14 25.
+If there are multiple bottom-most nodes for a horizontal distance from root,
+then print the later one in level traversal. For example, in the below diagram, 
+3 and 4 are both the bottommost nodes at horizontal distance 0, we need to print 4.
+
+                      20
+                    /    \
+                  8       22
+                /   \     /   \
+              5      3 4     25
+                     /    \      
+                 10       14
+
+For the above tree the output should be 5 10 4 14 25.
+ 
+
+Example 1:
+
+Input:
+       1
+     /   \
+    3     2
+Output: 3 1 2
+Explanation:
+First case represents a tree with 3 nodes
+and 2 edges where root is 1, left child of
+1 is 3 and right child of 1 is 2.
+
+Example 2:
+
+Input:
+         10
+       /    \
+      20    30
+     /  \
+    40   60
+Output: 40 20 60 30
+*/
+
+//Solution in JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}
+
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Tree g = new Tree();
+			    ArrayList <Integer> res = g.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        }
+	}
+}
+
+// } Driver Code Ends
+
+
+class Pair
+{
+    int first,second;
+    Pair()
+    {
+        
+    }
+    Pair(int f,int s)
+    {
+        first = f;
+        second = s;
+    }
+}
+
+class Tree
+{
+ 
+    
+    void bottom(Node node,int col,int row,TreeMap<Integer,Pair> map)
+    {
+        if(node == null)
+            return;
+        
+        if(!map.containsKey(col))
+        {
+            map.put(col,new Pair(node.data,row));
+        }
+        else if(map.get(col).second <= row)
+        {
+            map.put(col,new Pair(node.data,row));
+        }
+        bottom(node.left,col-1,row+1,map);
+        bottom(node.right,col+1,row+1,map);
+    }
+    
+    // Method that returns the bottom view.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        TreeMap<Integer,Pair> map = new TreeMap<>();
+        bottom(root,0,0,map);
+        ArrayList<Integer> out = new ArrayList<>();
+        for(Map.Entry<Integer,Pair> m : map.entrySet())
+        {
+            out.add((int)m.getValue().first);
+        }
+        return out;
+    }
+}
+
+/*
+Time Complexity: O(N)
+Auxiliary Space: O(N)
+*/
