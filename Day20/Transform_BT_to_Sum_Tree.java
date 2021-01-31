e9b881afa76cbc389229fcbd3a562f2b07e355b8
vamsi687 <31959666+vamsi687@users.noreply.github.com>
2021-01-31 20:22:28 +0530

    Transform Normal Binary tree to it's Sum Binary Tree

---
+++ Day20/Transform_BT_to_Sum_Tree.java
@@ -0,0 +1,161 @@
+/*
+Question :
+Given a Binary Tree of size N , where each node can have positive or negative values. Convert this to a tree where each node contains the sum of the left and right sub trees of the original tree. The values of leaf nodes are changed to 0.
+
+Example 1:
+
+Input:
+             10
+          /      \
+        -2        6
+       /   \     /  \
+     8     -4   7    5
+
+Output:
+            20
+          /    \
+        4        12
+       /  \     /  \
+     0     0   0    0
+
+Explanation:
+
+           (4-2+12+6)
+          /           \
+      (8-4)          (7+5)
+       /   \         /  \
+      0     0       0    0
+     
+*/
+
+//Solution in JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+        	    g.toSumTree(root);
+        	    printInorder(root);
+        	    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+class Tree{
+    
+    int sum(Node node)
+    {
+        if(node == null)
+            return 0;
+        //Store old data   
+        int oldData = node.data;
+        //Update the node data with it's child sum
+        node.data = sum(node.left)+sum(node.right);
+        //As the current node is also a subtree for the recursive function
+        //add the current node data and return
+        return oldData+node.data;
+    }
+    
+    public void toSumTree(Node root){
+        //Return the root sum
+         int rootSum = sum(root);
+    }
+}
+
+/*
+Time Complexity : O(N)
+Auxiliary Space Complexity : O(H)
+*/
