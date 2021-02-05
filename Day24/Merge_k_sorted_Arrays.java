1f676a88b674901dfd4bf759d25fc78094d299c8
vamsi687 <31959666+vamsi687@users.noreply.github.com>
2021-02-05 21:58:35 +0530

    Merge k Sorted Arrays using Priority Queue

---
+++ Day24/Merge_k_sorted_Arrays.java
@@ -0,0 +1,84 @@
+/*
+Question :
+Given K sorted arrays arranged in the form of a matrix of size K*K. The task is to merge them into one sorted array.
+Example 1:
+
+Input:
+K = 3
+arr[][] = {{1,2,3},{4,5,6},{7,8,9}}
+Output: 1 2 3 4 5 6 7 8 9
+Explanation:Above test case has 3 sorted
+arrays of size 3, 3, 3
+arr[][] = [[1, 2, 3],[4, 5, 6], 
+[7, 8, 9]]
+The merged list will be 
+[1, 2, 3, 4, 5, 6, 7, 8, 9].
+Example 2:
+
+Input:
+K = 4
+arr[][]={{1,2,3,4}{2,2,3,4},
+         {5,5,6,6},{7,8,9,9}}
+Output:
+1 2 2 2 3 3 4 4 5 5 6 6 7 8 9 9 
+Explanation: Above test case has 4 sorted
+arrays of size 4, 4, 4, 4
+arr[][] = [[1, 2, 2, 2], [3, 3, 4, 4],
+[5, 5, 6, 6]  [7, 8, 9, 9 ]]
+The merged list will be 
+[1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 
+6, 6, 7, 8, 9, 9 ].
+*/
+
+//Solution in JAVA
+
+import java.util.*;
+import java.io.*;
+
+class GFG{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t > 0){
+			int n = sc.nextInt();
+			int[][] a = new int[n][n];
+			for(int i = 0; i < n; i++)
+				for(int j = 0; j < n; j++)
+					a[i][j] = sc.nextInt();
+			Solution T = new Solution();
+			ArrayList<Integer> arr= T.mergeKArrays(a, n);
+			for(int i = 0; i < n*n ; i++)
+			    System.out.print(arr.get(i)+" ");
+		    System.out.println();
+		    
+		    t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    public static ArrayList<Integer> mergeKArrays(int[][] arrays,int k) 
+    {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=0;i<k;i++)
+        {
+            for(int j=0;j<k;j++)
+            {
+                queue.add(arrays[i][j]);
+            }
+        }
+        ArrayList<Integer> out = new ArrayList<>();
+        for(int i=0;i<k;i++)
+        {
+            for(int j=0;j<k;j++)
+            {
+                out.add(queue.peek());
+                queue.poll();
+            }
+        }
+        return out;
+    }
+}
